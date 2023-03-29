bd1403b83814cf3b267a84c10f7273d515a584d4
nemanjamalesija <malesija13@gmail.com>
2023-03-29 21:52:56 +0200

    Setup server

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,19 @@
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
+import { data } from './constants/data';
+
+const app = express();
+const server = http.createServer(app);
+app.use(cors());
+
+app.get('/api/movies', (req, res) => {
+  const moviesJSON = JSON.stringify(data);
+  console.log(moviesJSON);
+  res.setHeader('Content-Type', 'application/json');
+  res.end(moviesJSON);
+});
+
+server.listen(3001, () => {
+  console.log('Server listening on port 3001');
+});
