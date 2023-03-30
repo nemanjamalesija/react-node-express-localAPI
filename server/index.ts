861901e8083a127c633bceab6a7317bdb5a28679
nemanjamalesija <malesija13@gmail.com>
2023-03-30 22:18:03 +0200

    Add get method

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,27 @@
-import express from 'express';
+import express, { json } from 'express';
 import http from 'http';
 import cors from 'cors';
+import fs from 'fs';
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(json());
 app.use(cors());
+
+const tours = fs
+  .readFileSync('./constants/dev-data/data/tours-simple.json')
+  .toString();
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 
 server.listen(3001, () => {
   console.log('Server listening on port 3001');
