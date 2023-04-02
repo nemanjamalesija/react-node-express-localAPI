ff59ea9462e8af537f676d4f4c7182989ef159e5
nemanjamalesija <malesija13@gmail.com>
2023-04-03 00:35:57 +0200

    Test param middleware

--- server/routes/toursRoutes.ts
+++ server/routes/toursRoutes.ts
@@ -4,6 +4,11 @@ import { addTour, getRoutes, checkBody } from '../handlers/toursHandlers';
 
 const router = express.Router();
 
+router.param('id', (req, res, next, val) => {
+  console.log(`id is ${val}`);
+  next();
+});
+
 router.route('/').get(getRoutes).post(checkBody, addTour);
 
 export { router };
