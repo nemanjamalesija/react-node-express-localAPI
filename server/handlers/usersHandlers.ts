73c418be22e9f1d0caf90bff5df8c27a34ac2782
nemanjamalesija <malesija13@gmail.com>
2023-04-03 00:33:32 +0200

    Add user handlers

---
+++ server/handlers/usersHandlers.ts
@@ -0,0 +1,38 @@
+const getAllUsersHandler = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  });
+};
+const createUsersHandler = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  });
+};
+
+export {
+  getAllUsersHandler,
+  getUser,
+  createUsersHandler,
+  updateUser,
+  deleteUser
+};
